727a1b16e0276df48cb6a113d139970dd948c1e8
Carmen <elv-carmen.prtll@eduge.ch>
2017-04-05 03:47:35 +0200

    Cambie el HTML para la página.

--- formulaireEsig/views.py
+++ formulaireEsig/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. Es donde ponemos la logica de nuetsra app, toma info del model y pasa a una view
+
+#def uti_list(request):
+    #if request.method == "POST":
+      #  form = proForm(request.POST)
+      #  post = form.save(commit=False)
+       # post.save()
+  #  else:
+      #  form = proForm()
+   # return render(request, 'esigapp/formB.html', {'form': form})
+ #   return render(request, 'formulaireEsig/uti_list.html', {})
+def uti_list(request):
+    return render(request, 'formulaireEsig/uti_list.html', {})
